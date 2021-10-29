9be7922aff81c539157bf0880b3f959290fe0bd1
Vaughan Ling <heavypoly@gmail.com>
2021-10-28 21:26:24 -0700

    process_mouse_event added pressure

--- platform/android/java_godot_lib_jni.cpp
+++ platform/android/java_godot_lib_jni.cpp
@@ -284,7 +284,7 @@ void touch_preprocessing(JNIEnv *env, jclass clazz, jint input_device, jint ev, 
 	}
 
 	if ((input_device & AINPUT_SOURCE_MOUSE) == AINPUT_SOURCE_MOUSE) {
-		input_handler->process_mouse_event(ev, buttons_mask, points[0].pos, vertical_factor, horizontal_factor);
+		input_handler->process_mouse_event(ev, buttons_mask, points[0].pos, vertical_factor, horizontal_factor, pressure);
 	} else {
 		input_handler->process_touch(ev, pointer, points);
 	}
