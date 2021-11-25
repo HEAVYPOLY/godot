8a6ae6d85043d0cae78afdbbf3bf72ffa3b32010
Vaughan Ling <heavypoly@gmail.com>
2021-11-24 16:36:00 -0800

    Add X11 mousebutton pressure

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -2667,7 +2667,11 @@ void OS_X11::process_xevents() {
 						last_click_pos = Point2(event.xbutton.x, event.xbutton.y);
 					}
 				}
-
+				if (xi.pressure_supported) {
+					mb->set_pressure(xi.pressure);
+				} else {
+					mb->set_pressure((get_mouse_button_state() & (1 << (BUTTON_LEFT - 1))) ? 1.0f : 0.0f);
+				}
 				input->parse_input_event(mb);
 
 			} break;
